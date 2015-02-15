ae65416290fa67f7a887d4dac50df417fb20235c
Alan Mond <alan.mond@gmail.com>
2015-02-14 23:46:29 -0800

    configured simple_form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215070610) do
+ActiveRecord::Schema.define(version: 20150215073526) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150215070610) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "links", force: :cascade do |t|
@@ -46,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150215070610) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
