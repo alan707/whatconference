d7d5389c3fc40a3af2b8586ff2d56d182547fd36
Julien Vanier <jvanier@gmail.com>
2015-03-06 16:16:07 -0500

    More radar fun

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -10,4 +10,13 @@ module BootstrapHelper
     ALERT_CSS_CLASSES[type]
   end
 
+  # Use as the options of any *_tag helper like
+  # content_tag :span, "Test", tooltip_popup("Help")
+  def tooltip_popup(message, direction = :top)
+    { :data => { :tooltip => true,
+              :placement => direction,
+              :container => 'body' },
+      :title => message,
+    }
+  end
 end
